581c8461387f1ae28a8e52db62054ff2b87cd8b3
Russell Osborne <russell@burningpony.com>
2011-10-20 18:03:34 -0400

    Inital Commit

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+require 'etc'  
+  get '/' do
+    path =  Etc.getpwuid.dir 
+    pow_path = File.join(path,'.pow')
+    @apps = []
+    @apps =  Dir.entries(pow_path).select {|entry| File.directory? File.join(pow_path,entry) and !(entry =='.' || entry == '..'|| entry == 'smack') }
+    @apps.collect! { |app| app = {:name => app, :title => app.capitalize }}
+    erb :index
+  end
+  
