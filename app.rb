d1730c33d8e4c643dd3d1fe4a866090cfb2951f0
Russell Osborne <russell@burningpony.com>
2011-10-20 21:17:23 -0400

    Adding Pow Server Restart

--- app.rb
+++ app.rb
@@ -1,10 +1,18 @@
 require 'sinatra'
 require 'etc'  
-  get '/' do
-    path =  Etc.getpwuid.dir 
-    pow_path = File.join(path,'.pow')
-    @apps = []
-    @apps =  Dir.entries(pow_path).select {|entry| File.directory? File.join(pow_path,entry) and !(entry =='.' || entry == '..'|| entry == 'smack') }
-    @apps.collect! { |app| app = {:name => app, :title => app.capitalize }}
-    erb :index
-  end
+path =  Etc.getpwuid.dir 
+pow_path = File.join(path,'.pow')
+
+get '/' do
+  @apps = []
+  @apps =  Dir.entries(pow_path).select {|entry| File.directory? File.join(pow_path,entry) and !(entry =='.' || entry == '..'|| entry == 'smack') }
+  @apps.collect! { |app| app = {:name => app, :title => app.capitalize }}
+  erb :index
+end
+
+get '/restart/:app' do |app|
+  app_path = File.join(pow_path,app,'tmp')
+  FileUtils.mkdir_p(File.join(pow_path,app,'tmp'))
+  %x{touch #{app_path}} 
+  redirect '/'
+end
